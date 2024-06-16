481a8d720cf80a10b206060d09175cb21f6ddbe4
ArthurNathaniel <nathanielk.larthur@gmail.com>
2024-06-16 03:00:23 +0000

    updates

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include 'cdn.php' ?>
+    <link rel="stylesheet" href="./css/base.css">
+</head>
+
+<body>
+    <div class="index_all">
+        <div class="top_two_all">
+            <div class="top_one">
+                <div class="top_first">
+                    <h1>Bringing Your Ideas To Life Through Web Development</h1>
+                    <div class="hire_btn">
+                        <a href="">
+                            <button>Hire Me <span>🖖</span></button>
+                        </a>
+                    </div>
+                </div>
+                <div class="top_grid_three">
+                    <div class="top_box">
+                        <h1>4+</h1>
+                        <p>Years Experience</p>
+                    </div>
+                    <div class="top_box yellow">
+                        <h1>54+</h1>
+                        <p>Handled Project</p>
+                    </div>
+                    <div class="top_box pink">
+                        <h1>50+</h1>
+                        <p>Clients</p>
+                    </div>
+                </div>
+            </div>
+            <div class="top_two">
+                <div class="hello">
+                    <p>Nathstack</p>
+                    <span><i class="fa-solid fa-bars-staggered"></i></span>
+                </div>
+                <div class="profile_grid">
+                    <div class="profile"></div>
+                    <div class="profile_details">
+                        <div class="name">
+                            <span>Name:</span>
+                            Nathaniel K.L Arthur
+                        </div>
+                        <div class="based">
+                           <div class="based_flex">
+                           <span>Based in:</span>
+                           <p>Kumasi - Ghana</p>
+                           </div>
+                           <img src="./images/location.png" alt="">
+                        </div>
+                        <div class="social">
+                            <a href="" class="blue"><i class="fa-brands fa-linkedin"></i></a>
+                            <a href=""><i class="fa-brands fa-github"></i></a>
+                            <a href=""><i class="fa-brands fa-x-twitter"></i></a>
+                            <a href=""><i class="fa-brands fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
